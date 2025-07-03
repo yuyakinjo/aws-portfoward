dbac1be42e243d12f8d023c635df4b3cf8303a1b
GitHub Action <action@github.com>
2025-07-03 07:25:43 +0000

    chore: bump version to v2.2.20

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // This file is auto-generated during build process
 // Do not edit manually
 
-export const VERSION = "2.2.19";
+export const VERSION = "2.2.20";
 export const PACKAGE_NAME = "ecs-pf";
